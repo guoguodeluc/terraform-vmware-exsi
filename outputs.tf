01780d98411277879b9f5a1266de5b31c6c826ff
guoguodeluc <505759351@qq.com>
2024-07-26 13:52:50 +0800

    update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,14 @@
-#output "virtual_machine_ip" {
-#  value = vsphere_virtual_machine.vm.default_ip_address
-#}
-output "vm_ip_addresses" {
-  value = [for vm in vsphere_virtual_machine.vm : vm.default_ip_address]
+# 输出虚拟机的名称和IP地址
+output "vm_info" {
+  value = { for vm in vsphere_virtual_machine.vm : vm.name => vm.default_ip_address }
+  description = "vm name and ip"
 }
+
+
+## output "vm_ip_addresses" {
+##   value = [for vm in vsphere_virtual_machine.vm : vm.default_ip_address]
+## }
+## 
+## output "vm_names" {
+##   value = [for vm in vsphere_virtual_machine.vm : vm.name]
+## }
